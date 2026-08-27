ac87e4623ca0f6cf2f2c8b06758a0d592a087cc6
sjhan12 <sjhan1206@gmail.com>
2026-08-27 10:59:50 +0900

    Add files via upload

--- selfdrive/car/card.py
+++ selfdrive/car/card.py
@@ -105,7 +105,10 @@ class Car:
         with car.CarParams.from_bytes(cached_params_raw) as _cached_params:
           cached_params = _cached_params
 
-      fixed_fingerprint = (self.params.get("CarPlatformBundle") or {}).get("platform", None)
+      # G80 RG3 HDA2 retrofit: force the known CAN-FD platform because this vehicle/retrofit
+      # is not covered by the stock fingerprint set.
+      # fixed_fingerprint = (self.params.get("CarPlatformBundle") or {}).get("platform", None)
+      fixed_fingerprint = "GENESIS_G80_2ND_GEN_FL"
       init_params_list_sp = sunnypilot_interfaces.initialize_params(self.params)
 
       self.CI = get_car(*self.can_callbacks, obd_callback(self.params), alpha_long_allowed, is_release, cached_params,
